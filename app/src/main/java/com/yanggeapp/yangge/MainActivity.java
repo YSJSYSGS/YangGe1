20a1b7929c24d41e5e16c0f40cf7ffc412b5f40d
YSJSYSGS <2281728250@qq.com>
2017-11-08 17:24:33 +0800

    Initial commit

---
+++ app/src/main/java/com/yanggeapp/yangge/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yanggeapp.yangge;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null){
+            actionbar.hide();
+        }
+    }
+}
